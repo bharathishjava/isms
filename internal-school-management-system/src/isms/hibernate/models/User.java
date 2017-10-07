9101396860235bb7261b31fa01f315a7c4351a4c
bharathishjava <32603158+bharathishjava@users.noreply.github.com>
2017-10-08 03:37:55 +0530

    x
    first commit

---
+++ internal-school-management-system/src/isms/hibernate/models/User.java
@@ -0,0 +1,55 @@
+package isms.hibernate.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_details")
+public class User {
+	@Id
+	@Column(name="username")
+	private String userName;
+	@Column(name="password")
+	private String password;
+	@Column(name="usertype")
+	private String userType;
+	@Column(name="useremail")
+	private String email;
+	@Column(name="userphone")
+	private String phoneNumber;
+	
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+}
